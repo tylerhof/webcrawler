dc7b7b8fa009c9380da1a32a4d8a2252f9f1fd58
tylerhof <tyler.hofmeister@gmail.com>
2023-03-23 16:53:32 +0000

    revert broken playwright code (might be python 3.11 version dependent) and add functionality to filter (and return) links for more targeted scraping

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -15,14 +15,19 @@ from webcrawler.utils import GetDomain
 
 class Scrapy(Functor):
 
-    def __init__(self, settings = {'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'}, policy: ExceptionHandler = Safe()):
+    def __init__(self, settings={'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+                                 'DOWNLOAD_HANDLERS': {
+                                     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+                                     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler", }},
+                 policy: ExceptionHandler = Safe()):
         super().__init__(policy)
         self.process = CrawlerProcess(settings)
 
     def apply(self, input, **kwargs):
-        results = []
+        results = set()
+
         def crawler_results(signal, sender, item, response, spider):
-            results.append(item)
+            [results.add(link) for link in item['links']]
 
         dispatcher.connect(crawler_results, signal=signals.item_scraped)
 
@@ -30,59 +35,55 @@ class Scrapy(Functor):
             [self.process.crawl(spider) for spider in input]
         else:
             self.process.crawl(input)
-            
+
         self.process.start()
-        return results
+        return list(results)
+
 
 def process_links(links):
     for link in links:
         link.url = url_query_cleaner(link.url)
         yield link
 
+
 class CrawlSpiderSupplier(Functor):
 
-
-    def __init__(self, parse_links_getter = None, policy: ExceptionHandler = Unwrap()):
+    def __init__(self, parse_links_getter=None, policy: ExceptionHandler = Unwrap()):
         super().__init__(policy)
         if parse_links_getter:
             self.parse_links_getter = parse_links_getter
         else:
             self.parse_links_getter = self.default_parse_links_getter
 
-    def default_parse_links_getter(self, input):
-        link_extractor = LinkExtractor(allow=[re.escape(input['allowed_domains'][0])])
+    def default_parse_links_getter(self, input, **kwargs):
+        link_extractor = self.get_link_extractor(input, **kwargs)
         return lambda self, response: {'url': response.url,
                                        'body': response.body,
                                        'links': [link.url for link in link_extractor.extract_links(response)]}
+
+    def get_link_extractor(self, input, **kwargs):
+        if 'allow' in kwargs:
+            return LinkExtractor(allow=kwargs['allow'])
+        else:
+            return LinkExtractor(allow=[re.escape(kwargs['allowed_domains'][0])])
+
     def apply(self, input, **kwargs):
-        link_extractor = LinkExtractor(
-                        allow=[re.escape(input['allowed_domains'][0])])
-        parse_links = self.parse_links_getter(input)
+        link_extractor = self.get_link_extractor(input, **kwargs)
+        parse_links = self.parse_links_getter(input, **kwargs)
         return type("AnonymousSpiderClass",
                     (CrawlSpider,),
-                    {'name' : input['name'],
-                    'allowed_domains' : input['allowed_domains'],
-                    'start_urls' : input['start_urls'],
-                    'rules' : (Rule(link_extractor,
-                        process_links=process_links,
-                        callback='parse_item',
-                        follow=True),),
+                    {'name': kwargs['name'],
+                     'allowed_domains': kwargs['allowed_domains'],
+                     'start_urls': kwargs['start_urls'],
+                     'rules': (Rule(link_extractor,
+                                    process_links=process_links,
+                                    callback='parse_item',
+                                    follow=True),),
                      'parse_item': parse_links,
-                     'custom_settings' : {'DOWNLOAD_DELAY': 2,
-                                           'RANDOMIZE_DOWNLOAD_DELAY': False,
-                                          },
-                     '_build_request' : self._build_request
-                    })
-
-    def _build_request(self, rule_index, link):
-        return Request(
-            url=link.url,
-            callback=CrawlSpider._callback,
-            errback=CrawlSpider._errback,
-            meta=dict(rule=rule_index, link_text=link.text,
-                      playwright = True,
-                      playwright_include_page=True,
-                      playwright_page_methods =[PageMethod('wait_for_timeout', 10000)],))
+                     'custom_settings': {'DOWNLOAD_DELAY': 2,
+                                         'RANDOMIZE_DOWNLOAD_DELAY': False,
+                                         }
+                     })
 
 class WebCrawler(Functor):
 
--- webcrawler/example.py
+++ webcrawler/example.py
@@ -1,61 +1,70 @@
 import re
 from scrapy.linkextractors import LinkExtractor
 
-from webcrawler.crawler import CrawlSpiderSupplier, Scrapy, WebCrawler
-from webcrawler.utils import GetDomain, Rest, GetRestJson
+from webcrawler.crawler import CrawlSpiderSupplier, Scrapy
+from webcrawler.utils import GetDomain, GetRestJson
 
-web_crawler = Scrapy(settings = {'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-                                 'DOWNLOAD_HANDLERS' : {"http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-                                                        "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler"},
-                                'TWISTED_REACTOR' : "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
-                                 'PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT' : 90000})
+web_crawler = Scrapy(settings={'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+                               'DOWNLOAD_HANDLERS': {
+                                   "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+                                   "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler"},
+                               'TWISTED_REACTOR': "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+                               'PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT': 90000,
+                               'DEPTH_LIMIT': 1})
 
 parse_links = lambda input_dict: lambda self, response: {'url': response.url,
-                            'body': response.body,
-                            'links': [link.url for link in LinkExtractor(allow=[re.escape(input_dict['allowed_domains'][0])]).extract_links(response)]}
+                                                         'body': response.body,
+                                                         'links': [link.url for link in LinkExtractor(allow=[re.escape(
+                                                             input_dict['allowed_domains'][0])]).extract_links(
+                                                             response)]}
 
 supplier = CrawlSpiderSupplier()
 
+#citadel_spider = supplier(None, name='citadel',
+#                          allowed_domains=['www.citadel.com'],
+#                          start_urls=['https://www.citadel.com/careers/open-opportunities/investing/',
+#                                      'https://www.citadel.com/careers/open-opportunities/engineering/',
+#                                      'https://www.citadel.com/careers/open-opportunities/quantitative-research/',
+#                                      'https://www.citadel.com/careers/open-opportunities/business-operations/'])
 
-
-test_spider = supplier({'name' : 'test',
-                        "allowed_domains" : ["webscraper.io"],
-                        "start_urls" : ["https://webscraper.io/test-sites/e-commerce/allinone"]})
-citadel_spider = supplier({'name' : 'citadel',
-                           "allowed_domains" : ['www.citadel.com'],
-                           "start_urls" : ['https://www.citadel.com/careers/open-opportunities/investing/',
-                                           'https://www.citadel.com/careers/open-opportunities/engineering/',
-                                           'https://www.citadel.com/careers/open-opportunities/quantitative-research/',
-                                           'https://www.citadel.com/careers/open-opportunities/business-operations/']})
+citadel_spider = supplier(None, name='citadel',
+                          allowed_domains=['www.citadel.com'],
+                          start_urls=['https://www.citadel.com/careers/open-opportunities/investing/'])
 domain_getter = GetDomain()
 
 rest_getter = GetRestJson()
 
 jane_street = rest_getter('https://www.janestreet.com/jobs/main.json')
-mlp = rest_getter('https://corsanywhere-mlp.herokuapp.com/https://wd5-services1.myworkday.com/ccx/service/customreport2/mlp/ISU_Sullivan_JobPostings/INT_External_Job_Postings_RaaS?format=json',
-                  headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0',
-                           'Origin': 'https://www.mlp.com',
-                                                                                                                                                                                                          'Authorization': 'Basic SVNVX1N1bGxpdmFuX0pvYlBvc3RpbmdzOjVjWWJzVDZVT0dTZFZwY3dnanpW'})
+# mlp = rest_getter('https://corsanywhere-mlp.herokuapp.com/https://wd5-services1.myworkday.com/ccx/service/customreport2/mlp/ISU_Sullivan_JobPostings/INT_External_Job_Postings_RaaS?format=json',
+#                  headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0',
+#                           'Origin': 'https://www.mlp.com',
+#                           'Authorization': 'Basic SVNVX1N1bGxpdmFuX0pvYlBvc3RpbmdzOjVjWWJzVDZVT0dTZFZwY3dnanpW'})
 
 
-jane_street_spider = supplier({'name' : 'jane',
-                               "allowed_domains" : [domain_getter('https://www.janestreet.com/join-jane-street/open-roles/?type=experienced-candidates').value,
-                                                    domain_getter('https://www.janestreet.com/join-jane-street/open-roles/?type=experienced-candidates').value],
-                               "start_urls" : ['https://www.janestreet.com/join-jane-street/open-roles/?type=experienced-candidates',]})
+jane_street_spider = supplier(None,
+                              name='jane',
+                              allowed_domains=[domain_getter(
+                                  'https://www.janestreet.com/join-jane-street/open-roles/?type=experienced-candidates').value,
+                                               domain_getter(
+                                                   'https://www.janestreet.com/join-jane-street/open-roles/?type=experienced-candidates').value],
+                              start_urls=[
+                                  'https://www.janestreet.com/join-jane-street/open-roles/?type=experienced-candidates', ])
 
-test_spider = supplier({'name' : 'test',
-                        "allowed_domains" : ["webscraper.io"],
-                        "start_urls" : ["https://webscraper.io/test-sites/e-commerce/allinone"]})
+capula_spider = supplier(None,
+                       name='capula',
+                       allowed_domains=[domain_getter('https://fsr.cvmailuk.com/capula/main.cfm').value],
+                       start_urls=['https://fsr.cvmailuk.com/capula/main.cfm', ],
+                       allow=['jobSpecific&jobId'])
 
-millenium_spider = supplier({'name' : 'Millenium',
-                               "allowed_domains" : [domain_getter('https://mlp.wd5.myworkdayjobs.com/mlpcareers/job/London---50-Berkeley/Consumer-Staples-Analyst_REQ-15351').value,
-                                                    domain_getter('https://www.mlp.com/job-listings/').value],
-                               "start_urls" : ['https://www.mlp.com/job-listings/',]})
+#results = web_crawler([test_spider])
 
-results = web_crawler([millenium_spider])
 k = 2
 
+#results = web_crawler([jane_street_spider])
 
-results = web_crawler([jane_street_spider])
 
-results = web_crawler([citadel_spider])
+test_spider = supplier(None, name='test',
+                       allowed_domains=[domain_getter('http://quotes.toscrape.com/author/Albert-Einstein/').value],
+                       start_urls=["http://quotes.toscrape.com/author/Albert-Einstein/"])
+
+results = web_crawler([capula_spider])
